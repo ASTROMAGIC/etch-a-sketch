543f35f33fc5c179b60090ace72a793b4794786f
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-07-01 16:10:53 -0700

    catching up
    lost the context of these changes, will reassess on todays work

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
         container.appendChild(gridItem);
     }
 })
+
+// I understand this code in principle, an event listener that waits for the dom to load and uses a for loop to create a 16x16 grid system. The grid system should stop when it reaches a certain size indicated by the gridSize variable, these grid items created by the the foor loop are then appended to the grid-container element. 
+
+// there is a way to create these grids by creating a function that facilitates their creation. 
+
+// in theory I could have a function to create the grid, clear the grid and refresh the grid
