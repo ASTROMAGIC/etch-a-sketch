d2ae9902fc07d558a2ef3b457248d4d2e1637856
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-07-01 17:28:53 -0700

    reworked board function and size function
    functions populate board by size and input and will throw error messages if the input is too big. squares will also be removed upon lowering the size of the grid

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-// restarting js file from scratch since previous loop and logic did not work for what I was trying to do. 
 
-let board = document.querySelector('.board')
-board.style.gridTemplateColumns = "repeat (16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
 
-// note that when I create create variables and styles for certain elements, they should be grouped together for better readablity
+// this is a for loop to create the square divs for the baord up to a limit of 256, which is essentially a 16x16 grid. The style elements are for you to see each style.
 
-// I created a variable for my baord and styled it with gridTemplateRows and Columns to repeat 16 times. 
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr))`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr))`;
 
-for(let i =0; i<256; i++) {
-    let sqaure = document.createElement('div')
-    sqaure.style.backgroundColor= 'blue'
-    board.insertAdjacentElement('beforeend', sqaure)
+    let amount = size * size;
+    for(let i =0; i<amount; i++) {
+        let sqaure = document.createElement('div')
+        sqaure.style.backgroundColor= 'blue';
+        board.insertAdjacentElement('beforeend', sqaure);
+    }
 }
 
-// this is a for loop to create the square divs for the baord up to a limit of 256, which is essentially a 16x16 grid. The style elements are for you to see each style
+// The create of the board divs have now been consolidated into a function that can be accessed whereever placed in this program.
+// created squares variable with a small for each function to remove the divs when decreasing the size of the board 
+
+populateBoard(16);
+
+// now we call the function to ensure all of our divs are created before we put our variables and loops into our function
+
+function changeSize(input){
+    if (input >= 2 && input <= 100){
+        populateBoard(input); 
+    } else {
+        console.log("Too many Squares!");
+    }    
+}
+
+// if else conditional written to ensure the sketch board doesn't crash when the input is too high.
+
+// we have created a function to change the size of our board which can be attached to the html file with a button onlick attachment. 
